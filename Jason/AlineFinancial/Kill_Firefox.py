47b72837dffd23704dd0b8adf2305df302b523c4
JasonSwan <37820464+JasonSwan@users.noreply.github.com>
2022-10-14 13:44:38 -0500

    Updating to kill geckodriver processes as well

--- Jason/AlineFinancial/Kill_Firefox.py
+++ Jason/AlineFinancial/Kill_Firefox.py
@@ -4,8 +4,10 @@ print(os.name)
 
 if(os.name=='nt'):
     os.system("taskkill /f /im geckodriver.exe /T")
+    os.system("taskkill /f /im firefox.exe /T")
 if(os.name=='posix'):
     os.system("sudo pkill -9 firefox")
+    os.system("sudo pkill -9 geckodriver")
 
 
 #sudo pkill -9 firefox
